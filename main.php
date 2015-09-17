25f85437cbc37ebc7c6419657ffdd56718fd20c7
iltempe <pratosmart@gmail.com>
2015-09-17 02:29:36 +0200

    fixing

--- main.php
+++ main.php
@@ -36,7 +36,7 @@ class mainloop{
 		$chat_id = $update["message"] ["chat"]["id"];
 		$user_id=$update["message"]["from"]["id"];
 		$location=$update["message"]["location"];
-		$reply_to_msg=$update["message"]["reply_to_msg"];
+		$reply_to_msg=$update["message"]["reply_to_message"];
 		
 		$this->shell($telegram, $db,$data,$text,$chat_id,$user_id,$location,$reply_to_msg);
 		$db = NULL;
